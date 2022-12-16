e46ac4f74b0fad79e7427df95d0cba5e83064eae
M. Raynoalief Ridwan Ar Rhojabi <118473186+reynoalief@users.noreply.github.com>
2022-12-16 15:18:38 +0700

    Update tugas4.js

--- week3/tugas4.js
+++ week3/tugas4.js
@@ -2,29 +2,34 @@ let result = "";
 
 function nilaiCheck(nilaiAwal, nilaiAkhir, dataArray) {
   let nilai = (nilaiAwal, nilaiAkhir, dataArray);
-  let sortNumber = dataArray.sort((a, b) => {
+
+  let filter = dataArray.filter((data) => {
+    return data > nilaiAwal && data < nilaiAkhir;
+  });
+
+  filter.sort((a, b) => {
     return a - b;
   });
+
   if (isNaN(nilaiAwal) && isNaN(nilaiAkhir)) {
     result = "semua nilai harus number";
   } else if (nilaiAwal > nilaiAkhir) {
     result = "Nilai akhir harus lebih besar dari nilai awal";
   } else if (dataArray.length < 5) {
     result = "Jumlah angka dalam dataArray harus lebih dari 5";
-  } else {
-    if (condition) {
-      
+  } else if (nilaiAwal < nilaiAkhir && dataArray.length > 5) {
+    if (filter.length > 0) {
+      return filter;
     } else {
-      let filter = dataArray.filter((data) => {
-        return data > 5 && data < 20;
-      });
-      console.log(filter);
+      result = "nilai tidak ditemukan";
     }
+  } else {
+    result = "eror logic";
   }
   return result;
 }
 
 console.log(nilaiCheck(5, 20, [2, 25, 4, 14, 17, 30, 8]));
-// console.log(nilaiCheck(15, 3, [2, 25, 4, 14, 17, 30, 8]));
-// console.log(nilaiCheck([2, 25, 4]));
-// console.log(nilaiCheck([2, 25, 4, 1, 30, 18]));
+console.log(nilaiCheck(15, 3, [2, 25, 4, 14, 17, 30, 8]));
+console.log(nilaiCheck(4, 17, [2, 25, 4]));
+console.log(nilaiCheck(5, 17, [2, 25, 4, 1, 30, 18]));
